334c4f00d404fe919f6a060f10f07d0b0c25184f
alocasia-polly <wendy-shaw@outlook.com>
2024-05-30 06:50:15 -0700

    Created calculator.js, defined variables and experimented with operands and concatenation

---
+++ lesson03/calculator.js
@@ -0,0 +1,15 @@
+let u = "2"
+let v = 1
+let w = 5
+let x = 8
+let y = 20
+
+console.log(v + w);
+console.log(w * x);
+console.log(y - (y / w));
+console.log((v - w) * (x - y));
+
+console.log(v + u);
+console.log(v - u);
+console.log(v * u);
+console.log(v / u);
